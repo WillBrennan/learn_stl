4a60f972690e8cad9b6eba626acee6161b057cad
WillBrennan <WillBrennan@users.noreply.github.com>
2017-12-06 22:08:18 +0000

    added unique_ptr

--- learn_stl/memory.h
+++ learn_stl/memory.h
@@ -1,10 +1,58 @@
 #pragma once
 
+#include <type_traits>
+
 namespace learn {
 
 template <class T>
 constexpr T* addressof(T& v) {
     return reinterpret_cast<T*>(&const_cast<char&>(reinterpret_cast<const volatile char&>(v)));
 }
+
+template <typename Object>
+struct default_delete {
+    static_assert(!std::is_function<Object>::value,
+                  "default_delete cannot be instantiated for function types");
+
+    constexpr default_delete() noexcept = default;
+
+    void operator()(Object* ptr) const noexcept {
+        static_assert(sizeof(ptr) > 0, "default_delete can not delete incomplete type");
+        static_assert(!std::is_void<Object>::value,
+                      "default_delete can not delete incomplete type");
+        delete ptr;
+    }
+};
+
+template <class T, class Deleter = default_delete<T>>
+class unique_ptr {
+  public:
+    using element_type = T;
+    using pointer = T*;
+    using deleter_type = Deleter;
+
+    unique_ptr() noexcept : pointer_(nullptr) {}
+    unique_ptr(pointer p) noexcept : pointer_(p) {}
+
+    void reset(pointer ptr = pointer()) noexcept {
+        if (pointer_) {
+            deleter_type()(pointer_);
+        }
+
+        if (ptr) {
+            pointer_ = ptr;
+        }
+    }
+
+    ~unique_ptr() { reset(); }
+
+    pointer operator->() const noexcept { return pointer_; }
+    typename std::add_lvalue_reference<T>::type operator*() const { return pointer_; }
+
+    explicit operator bool() const noexcept { return bool(pointer_); }
+
+  private:
+    pointer pointer_;
+};
 
 }  // namespace learn
