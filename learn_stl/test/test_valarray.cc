c8eb0572984d026d49e3746ad7c0910de5652699
WillBrennan <WillBrennan@users.noreply.github.com>
2017-12-28 16:27:23 +0000

    more tests for valarray

--- learn_stl/test/test_valarray.cc
+++ learn_stl/test/test_valarray.cc
@@ -10,7 +10,7 @@ TEST(valarray, Construction) {
     ASSERT_NO_THROW({ const auto val_a = valarray(0.3, 10); });
 }
 
-TEST(valarray, Addition) {
+TEST(valarray, Add) {
     using valarray = learn::valarray<double>;
     valarray val_a(0.35, 20);
     valarray val_b(0.232, 20);
@@ -18,5 +18,49 @@ TEST(valarray, Addition) {
     const valarray result = val_a + val_b;
 
     valarray val_expt(0.35 + 0.232, 20);
+    EXPECT_EQ(result, val_expt);
+}
+
+TEST(valarray, Subtract) {
+    using valarray = learn::valarray<double>;
+    valarray val_a(0.35, 20);
+    valarray val_b(0.232, 20);
+
+    const valarray result = val_a - val_b;
+
+    valarray val_expt(0.35 - 0.232, 20);
+    EXPECT_EQ(result, val_expt);
+}
+
+TEST(valarray, Multiply) {
+    using valarray = learn::valarray<double>;
+    valarray val_a(0.35, 20);
+    valarray val_b(0.232, 20);
+
+    const valarray result = val_a * val_b;
+
+    valarray val_expt(0.35 * 0.232, 20);
+    EXPECT_EQ(result, val_expt);
+}
+
+TEST(valarray, Divide) {
+    using valarray = learn::valarray<double>;
+    valarray val_a(0.35, 20);
+    valarray val_b(0.232, 20);
+
+    const valarray result = val_a / val_b;
+
+    valarray val_expt(0.35 / 0.232, 20);
+    EXPECT_EQ(result, val_expt);
+}
+
+TEST(valarray, Modulo) {
+    using valarray = learn::valarray<int>;
+    valarray val_a(423, 20);
+    valarray val_b(32, 20);
+
+    const valarray result = val_a % val_b;
+
+    valarray val_expt(423 % 32, 20);
     EXPECT_EQ(result, val_expt);
 }
